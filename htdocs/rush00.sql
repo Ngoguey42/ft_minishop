982fe8c763069f9ed5fe801d1731de8433e9b8dc
wide-aze <wide-aze@student.42.fr>
2015-03-25 11:27:33 +0100

    database ok

--- htdocs/rush00.sql
+++ htdocs/rush00.sql
@@ -36,10 +36,39 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lastname` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `zipcode` int(11) NOT NULL,
+  `zipcode` int(5) NOT NULL,
   `city` varchar(255) NOT NULL,
   `admin` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 INSERT INTO `rush00`.`categories` (`id`, `name`) VALUES (NULL, 'condoms'), (NULL, 'cockrings'), (NULL, 'dildos'), (NULL, 'lingeries'), (NULL, 'lubes');
+
+INSERT INTO `rush00`.`users` (`id`, `login`, `password`, `lastname`, `firstname`, `address`, `zipcode`, `city`, `admin`) VALUES (NULL, 'root', '4925da7da7a56260baf1c37925a8fa24e46ad8b107dcd21f44e39e4751bae1304fc70de7acb847ffa96126bb372de005f5320f1ede6f9df07c7d53f9c160f022', 'root', 'root', 'root', 69069, 'root', 1) ;
+
+INSERT INTO `rush00`.`items` (`id`, `category_id`, `name`, `price`) VALUES 
+(NULL, 1, 'name1', 1),
+(NULL, 1, 'name2', 1),
+(NULL, 1, 'name3', 1),
+(NULL, 1, 'name4', 1),
+(NULL, 1, 'name5', 1),
+(NULL, 2, 'name6', 1),
+(NULL, 2, 'name7', 1),
+(NULL, 2, 'name8', 1),
+(NULL, 2, 'name9', 1),
+(NULL, 2, 'name10', 1),
+(NULL, 3, 'name11', 1),
+(NULL, 3, 'name12', 1),
+(NULL, 3, 'name13', 1),
+(NULL, 3, 'name14', 1),
+(NULL, 3, 'name15', 1),
+(NULL, 4, 'name16', 1),
+(NULL, 4, 'name17', 1),
+(NULL, 4, 'name18', 1),
+(NULL, 4, 'name19', 1),
+(NULL, 4, 'name20', 1),
+(NULL, 5, 'name21', 1),
+(NULL, 5, 'name22', 1),
+(NULL, 5, 'name23', 1),
+(NULL, 5, 'name24', 1),
+(NULL, 5, 'name25', 1);
