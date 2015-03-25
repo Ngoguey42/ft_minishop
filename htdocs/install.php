fed9e8375c411892e33d8c44c403bdde47eca0cc
wide-aze <wide-aze@student.42.fr>
2015-03-25 10:03:11 +0100

    installation ok

--- htdocs/install.php
+++ htdocs/install.php
@@ -1,7 +1,13 @@
 <?php
-$ret = mysqli_connect("localhost:3306", "root", "qwerty");
-if (!$ret)
-	exit("Could not connect: ".mysqli_error($ret).PHP_EOL);
-echo "Connected successfully".PHP_EOL;
-mysqli_close($ret);
+if (!($mysqli = mysqli_connect("localhost:3306", "root", "qwerty")))
+	exit("Could not connect: ".mysqli_connect_error().PHP_EOL);
+if (!($queries = file_get_contents("rush00.sql")))
+	exit("Could not import database.".PHP_EOL);
+$queries = str_replace("\n", "", $queries);
+if (!(mysqli_multi_query($mysqli, $queries)))
+	echo "Could not create database.".PHP_EOL;
+else
+	echo "Database imported successfully".PHP_EOL;
+if (!(mysqli_close($mysqli)))
+	echo "Could not close connection.".PHP_EOL;
 ?>
