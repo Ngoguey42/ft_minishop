faa61495bfeee24b4b711787282eb4c7a0730e25
Ngoguey42 <ngoguey@student.42.fr>
2015-03-25 11:24:08 +0100

    demerde toi

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,32 +1,20 @@
 
+<!doctype html>
 <html>
 	<head>
 		<title>ft_minishop</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" type="text/css" href="ft_minishop.css">
+		<link rel="stylesheet" type="text/css" href="/ft_minishop.css">
 	</head>
 	<body>
 		<div class="main-box">
-			<div class="top-box">
-					<h1 class="site-title">
-						<a href="/">
-						ft_minishop
-						</a>
-					</h1>
-					<table>
-						<tr>
-							<th><a href="/list?cat=1">Condoms</a></th>
-							<th><a href="/list?cat=2">Cockrings</a></th>
-							<th><a href="/list?cat=3">Dildos</a></th>
-							<th><a href="/list?cat=4">Lingeries</a></th>
-							<th><a href="/list?cat=5">Lubes</a></th>
-						</tr>
-					</table>
+			<?php require("/header.html") ?>
+			<div class="content-box">
+				<?php
+				require("/main_page.php");
+				?>
 			</div>
-
-			<div>
-				Content <br> Content <br> Content <br> Content <br> Content <br> Content <br> Content <br> Content <br> Cont
-			</div>
+			<?php require("/footer.html") ?>
 		</div>
 	</body>
 </html>
